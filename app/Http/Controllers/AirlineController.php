0a1e776dca68a5351821cead9417df871e0b1250
Tóth Alexandra <alejandra.marianella.8@gmail.com>
2023-11-15 19:54:10 +0100

    huss és pöcc

---
+++ app/Http/Controllers/AirlineController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Airline;
+use Illuminate\Http\Request;
+
+class AirlineController extends Controller
+{
+    public function index(){
+        $airlines = response()->json(Airline::all());
+        return $airlines;
+    }
+
+    public function show($id){
+        $airline = response()->json(Airline::find($id));
+        return $airline;
+    }
+
+    public function store(Request $request){
+        $airline = new Airline();
+        $airline->name = $request->name;
+        $airline->country = $request->country;
+        
+        $airline->save();
+    }
+
+    public function update(Request $request, $id){
+        $airline = Airline::find($id);
+        $airline->name = $request->name;
+        $airline->country = $request->country;
+        $airline->save();
+    }
+
+    public function destroy(Request $request, $id){
+        Airline::find($id)->delete();
+    }
+}
